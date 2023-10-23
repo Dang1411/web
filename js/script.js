4804420669518877a7013acffb5f02d99e14d5b8
Dang1411 <148377085+Dang1411@users.noreply.github.com>
2023-10-23 09:19:33 +0700

    web

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// coding implementation
+
+var ck_email = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
+var ck_rating = /^[0-9]{0,5}$/;
+
+function validate(form){
+    var email = form.email.value;
+    var rating = form.rating.value;
+    var errors = [];
+
+    if(!ck_email.test(email)) {
+        errors[errors.length] = "You must enter a valid email address.";
+    }
+
+    if(!ck_rating.test(rating)) {
+        errors[errors.length] = "You must enter a valid rating";
+    }
+
+    if(errors.length > 0){
+        reportErrors(errors);
+        return false;
+    }
+    return true;
+}
+
+function reportErrors(errors){
+    var msg = "Please Enter Valide Data...\n";
+    for(var i=0; i<errors.length; i++){
+        var numError = i+1;
+        msg += "\n" + numError + ". " + errors[i];
+    }
+    alert(msg);
+}
+
+// end coding implementation
